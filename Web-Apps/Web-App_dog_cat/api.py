531928cbb518ddf8a03e7c561599c07b6e09889d
aspiazza <58825320+aspiazza@users.noreply.github.com>
2021-09-15 23:39:09 -0400

    Was able to return webpage and pass file through. Now need to run it through prediction and return the value back onto the template.

--- Web-Apps/Web-App_dog_cat/api.py
+++ Web-Apps/Web-App_dog_cat/api.py
@@ -1,6 +1,6 @@
 from starlette.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from keras.models import load_model
@@ -11,6 +11,7 @@ from icecream import ic
 app = FastAPI()
 app.mount("/Templates", StaticFiles(directory="Templates"), name="Templates")
 
+
 # model_dir = 'F:\\Saved-Models\\Dog-Cat-Models\\First_Generation_dog_cat_optuna.h5'
 # model = load_model(model_dir)
 
@@ -18,6 +19,12 @@ app.mount("/Templates", StaticFiles(directory="Templates"), name="Templates")
 @app.get('/')
 async def index():
     return RedirectResponse(url="/Templates/index.html")
+
+
+@app.post('/prediction_page')
+async def prediction_form(dogcat_img: bytes = Form(...)):
+    print(dogcat_img)
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='localhost', port=8000)
