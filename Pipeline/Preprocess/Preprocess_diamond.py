41b5e61bdb3ac5ec12b9e4d7a251511f4c5f4452
aspiazza <58825320+aspiazza@users.noreply.github.com>
2021-11-01 23:51:33 -0400

    Still figuring out the Preprocess_diamond.py. I need to find out how to process my labels and if I even have to. Will troubleshoot.

--- Pipeline/Preprocess/Preprocess_diamond.py
+++ Pipeline/Preprocess/Preprocess_diamond.py
@@ -16,10 +16,10 @@ def diamond_preprocess(data_dir):
     x = cleaned_data.drop(['price'], axis=1)  # Train data
     y = cleaned_data['price']  # Label data
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=99)
 
-    numerical_features = x_train.select_dtypes(include=['int64', 'float64']).columns
-    categorical_features = x_train.select_dtypes(include=['object']).columns
+    numerical_features = x_train.select_dtypes(include=['int64', 'float64']).columns.tolist()
+    categorical_features = x_train.select_dtypes(include=['object']).columns.tolist()
 
     numerical_transformer = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='median')),  # Fill in missing data with median
@@ -37,7 +37,30 @@ def diamond_preprocess(data_dir):
             ('cat', categorical_transformer, categorical_features)
         ])
 
-    x_train = preprocessor_pipeline.fit(x_train)
-    x_test = preprocessor_pipeline.fit(x_test)
+    # Fit to the training data
+    preprocessor_pipeline.fit(x_train)
+    ic(x_train.shape)
+    ic(y_train.head(5))
+    # preprocessor_pipeline.fit(y_train)
+
+    # Apply the pipeline to the training and test data
+    x_train_pipe = preprocessor_pipeline.transform(x_train)
+    x_test_pipe = preprocessor_pipeline.transform(x_test)
+    # y_train_pipe = preprocessor_pipeline.transform(y_train)
+    # y_test_pipe = preprocessor_pipeline.transform(y_test)
+
+    x_train = pd.DataFrame(data=x_train_pipe)
+    x_test = pd.DataFrame(data=x_test_pipe)
+    # y_train = pd.DataFrame(data=y_train_pipe)
+    # y_test = pd.DataFrame(data=y_test_pipe)
+
+    ic(x_train)
+    ic(type(x_train))
+    ic(x_test)
+    ic(type(x_test))
+    ic(y_train)
+    ic(type(y_train))
+    ic(y_test)
+    ic(type(y_test))
 
     return x_train, x_test, y_train, y_test
